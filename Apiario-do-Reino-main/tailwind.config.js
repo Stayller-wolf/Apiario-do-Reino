fdda693905ad152fb01df162f301f5aa09c9258a
rMaiaa <ronaldteofilomaia@gmail.com>
2024-11-13 18:12:00 -0300

    add paginas

---
+++ Apiario-do-Reino-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home":"url('/assets/abelha 1.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
