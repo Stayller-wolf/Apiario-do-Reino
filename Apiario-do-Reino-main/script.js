2243cb21f33e2603361a8bcafdbe3fb2ad45a4af
Stayller-wolf <carlosemiriele.matos01@gmail.com>
2024-11-20 02:22:12 -0300

    add menu clicavel em botton footer

---
+++ Apiario-do-Reino-main/script.js
@@ -0,0 +1,59 @@
+const menu= document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const carItemsContainer = document.getElementById("cart-items")
+const carTotal = document.getElementById("cart-total")
+const checkout = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+
+let cart = [];
+
+// Abrir o modal do carrinho
+cartBtn.addEventListener("click" , function() {
+    cartModal.style.display= "flex"
+})
+
+// Fechar o modal quando clicar fora
+cartModal.addEventListener("click" , function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click" , function(){
+    cartModal.style.display = "none"
+})
+
+
+menu.addEventListener("click" , function(event){
+    //console.log(event.target)
+     let parentButton = event.target.closest(".add-to-cart-btn")
+     
+     if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name,price)
+    
+     }
+})
+
+// Função para adicionar no carrinho 
+function addToCart(name,price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity +=1;
+        return;
+    }
+    
+ cart.push({
+    name,
+    price,
+    quantity: 1,
+ })
+
+}
