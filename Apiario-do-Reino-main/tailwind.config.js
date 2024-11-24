5b9686a159168123a9f9ddfe9888d9859d4fa8a3
rMaiaa <ronaldteofilomaia@gmail.com>
2024-11-24 16:29:41 -0300

    Configuração do Package.Json, Configuração da página Home, Configuração do Tailwind-Config

--- Apiario-do-Reino-main/tailwind.config.js
+++ Apiario-do-Reino-main/tailwind.config.js
@@ -1,16 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    fontFamily:{
-      'sans':['Roboto','sans-serif']
+  content: ["./Apiario-do-Reino-main/**/*.{html,js}"],
+  theme: { 
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
     },
     extend: {
-      backgroundImage:{
-        "home":"url('/assets/abelha 1.png')"
-      }
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        primary: '#FDE010',
+        secundary: '#000000'
+      },
+      backgroundImage: {
+        home: "url('/assets/abelha 1.png')",
+      },
     },
   },
+  variants: {
+    extend: {
+      
+    }
+  },
   plugins: [],
-}
-
+};
